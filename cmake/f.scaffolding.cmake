73100e167e4d543485def2bf8f4ce87edfa3c30d
Zheng Sun <fcwyzzr@163.com>
2026-04-29 10:36:57 +0800

    支持EXE工件的单元测试，支持无模块编译，重命名测试工件，调整cmake版本

--- cmake/f.scaffolding.cmake
+++ cmake/f.scaffolding.cmake
@@ -4,78 +4,86 @@ set(USE_CUDA OFF)
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-set(CMAKE_CUDA_STANDARD 20)
-set(CMAKE_CUDA_STANDARD_REQUIRED ON)
-set(CMAKE_CUDA_EXTENSIONS ON)
 
 function(scan_target name)
-    file(GLOB_RECURSE h src/*.h)
-    file(GLOB_RECURSE hpp src/*.hpp)
-    file(GLOB_RECURSE private_h src/*.private.h)
-    file(GLOB_RECURSE private_hpp src/*.private.hpp)
-    file(GLOB_RECURSE cpp src/*.cpp)
-    file(GLOB_RECURSE ixx src/*.ixx)
-    file(GLOB_RECURSE test src/*.test.cpp)
+    file(GLOB_RECURSE h         CONFIGURE_DEPENDS src/*.h)
+    file(GLOB_RECURSE hpp       CONFIGURE_DEPENDS src/*.hpp)
+    file(GLOB_RECURSE private_h CONFIGURE_DEPENDS src/*.private.h)
+    file(GLOB_RECURSE private_hpp CONFIGURE_DEPENDS src/*.private.hpp)
+    file(GLOB_RECURSE cpp       CONFIGURE_DEPENDS src/*.cpp)
+    file(GLOB         main      CONFIGURE_DEPENDS src/main.cpp)
+    file(GLOB_RECURSE ixx       CONFIGURE_DEPENDS src/*.ixx)
+    file(GLOB_RECURSE test      CONFIGURE_DEPENDS src/*.test.cpp)
 
-    file(GLOB_RECURSE cu src/*.cu)
-    file(GLOB_RECURSE test_cu src/*.test.cu)
+    file(GLOB_RECURSE cu        CONFIGURE_DEPENDS src/*.cu)
+    file(GLOB_RECURSE test_cu   CONFIGURE_DEPENDS src/*.test.cu)
 
-    set(MODULE_ENABLE OFF)
-    set(CUDA_ENABLE OFF)
-
-    if (ixx)
-        message(NOTICE "module detected")
-        set(MODULE_ENABLE ON)
+    if(cu)
+        if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+            set(CMAKE_CUDA_ARCHITECTURES 75 86 89 90)
+        endif()
+        enable_language(CUDA)
+        set(CMAKE_CUDA_STANDARD 20)
+        set(CMAKE_CUDA_STANDARD_REQUIRED ON)
+        set(CMAKE_CUDA_EXTENSIONS ON)
     endif ()
-    if (cu)
-        message(NOTICE "CUDA detected")
-        set(CUDA_ENABLE ON)
-    endif ()
+
 
     list(REMOVE_ITEM h ${private_h})
     list(REMOVE_ITEM hpp ${private_hpp})
-    list(REMOVE_ITEM cpp ${test})
+    list(REMOVE_ITEM cpp ${test} ${main})
     list(REMOVE_ITEM cu ${test_cu})
 
 
 
-    set(${name}_h ${h} PARENT_SCOPE)
-    set(${name}_hpp ${hpp} PARENT_SCOPE)
-    set(${name}_cu ${cu} PARENT_SCOPE)
-    set(${name}_cpp ${cpp} PARENT_SCOPE)
+    set(${name}_header ${h} ${hpp} PARENT_SCOPE)
+    set(${name}_main ${main} PARENT_SCOPE)
+    set(${name}_src ${cu} ${cpp} PARENT_SCOPE)
     set(${name}_ixx ${ixx} PARENT_SCOPE)
-    set(${name}_test ${test} PARENT_SCOPE)
-    set(${name}_test_cu ${test_cu} PARENT_SCOPE)
-
-    set(CUDA_ENABLE ${CUDA_ENABLE} PARENT_SCOPE)
-    set(MODULE_ENABLE ${MODULE_ENABLE} PARENT_SCOPE)
+    set(${name}_test ${test} ${test_cu} PARENT_SCOPE)
 endfunction()
 
 function(configure_target target)
     scan_target(${target})
-    target_include_directories(${target} PUBLIC src)
-    target_sources(${target}
-            PRIVATE ${${target}_cpp} ${${target}_cu}
+    get_target_property(target_type ${target} TYPE)
+    if (target_type STREQUAL "EXECUTABLE")
+        set(lib ${target}_lib)
+        target_sources(${target} PRIVATE ${${target}_main})
+        add_library(${lib} STATIC)
+        target_link_libraries(${target} PRIVATE ${lib})
 
-            PUBLIC FILE_SET ixx
-            TYPE CXX_MODULES
-            FILES ${${target}_ixx}
-            PUBLIC FILE_SET h
-            TYPE HEADERS
-            FILES ${${target}_h}
-            PUBLIC FILE_SET hpp
-            TYPE HEADERS
-            FILES ${${target}_hpp})
+    else ()
+        set(lib ${target})
+        list(APPEND ${target}_src ${${target}_main})
+    endif ()
+
+    if (${target}_src)
+        target_sources(${lib}
+                PRIVATE ${${target}_src})
+    endif ()
+    if (${target}_ixx)
+        target_sources(${lib}
+                PUBLIC FILE_SET ixx
+                TYPE CXX_MODULES
+                FILES ${${target}_ixx})
+    endif ()
+    if (${target}_header)
+        target_include_directories(${lib} PUBLIC src)
+        target_sources(${lib}
+                PUBLIC FILE_SET h
+                TYPE HEADERS
+                FILES ${${target}_header})
+    endif ()
+
 
     if (BUILD_TEST)
-        foreach (file ${${target}_test} ${${target}_test_cu})
+        foreach (file ${${target}_test})
             get_filename_component(name ${file} NAME_WLE)
             get_filename_component(name ${name} NAME_WLE)
 
-            set(test_exe_name "test-${target}-${name}")
+            set(test_exe_name "${target}-test__${name}")
             add_executable(${test_exe_name} ${file})
-            target_link_libraries(${test_exe_name} PRIVATE ${target})
+            target_link_libraries(${test_exe_name} PRIVATE ${lib})
         endforeach ()
     endif ()
-
 endfunction()
